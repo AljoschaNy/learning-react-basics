4eaf58dea57f17e06a25751369718f94c1f8ccd5
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-27 11:07:11 +0200

    Add props to articles

--- src/components/main/SectionIntroduction.tsx
+++ src/components/main/SectionIntroduction.tsx
@@ -1,11 +1,15 @@
-import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 import ShortLine from "./ShortLine.tsx";
 
-function SectionIntroduction(props: { sectionTitle: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; sectionDescription: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }) {
+type Props = {
+    sectionTitle: string,
+    sectionDescription: string
+}
+
+function SectionIntroduction({sectionTitle, sectionDescription} : Props) {
     return (
         <section className="section-introduction">
-            <h2>{props.sectionTitle}</h2>
-            <p>{props.sectionDescription}</p>
+            <h2>{sectionTitle}</h2>
+            <p>{sectionDescription}</p>
             <ShortLine />
         </section>
     )
